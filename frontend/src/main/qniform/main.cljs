fd576a5b443b72fa093711ff9f6a6411c14d8194
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-09-07 15:32:08 +0100

    refactor rules into separate ns

--- frontend/src/main/qniform/main.cljs
+++ frontend/src/main/qniform/main.cljs
@@ -2,97 +2,9 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rd]
             [malli.core :as m]
-            [meander.epsilon :as e]))
+            [qniform.rules :refer [rules get-schema get-xform]]))
 
-(def app-state (r/atom {:dropdown-selected :share-issue}))
-
-(defn ccy-amnt? [num]
-  (and (number? num)
-       (= num (/ (Math/round (* 100 num)) 100))))
-
-(def money (m/schema [:fn {:display "Money amount"
-                           :coercion :double}
-                      ccy-amnt?]))
-
-(defn round-2dp [num]
-  (/ (Math/round (* 100 num)) 100))
-
-(def share-issue-event
-  (m/schema [:map
-             [:event-id :string]
-             [:shares :int]
-             [:price-per-share money]
-             [:comment :string]]))
-
-(defn share-xform [event]
-  (e/match {:event event}
-    {:event {:event-id ?event-id
-             :shares ?shares
-             :price-per-share ?price-per-share
-             :comment ?comment}}
-    {:comment ?comment
-     :journal-entries [{:event-id ?event-id
-                        :dr-cr :credit
-                        :account :share-capital
-                        :currency "USD"
-                        :local-amount (round-2dp (* ?price-per-share ?shares))}
-                       {:event-id ?event-id
-                        :dr-cr :debit
-                        :account :cash
-                        :currency "USD"
-                        :local-amount (round-2dp (* ?price-per-share ?shares))}]}))
-
-(def invoice-payable-event
-  (m/schema [:map
-             [:event-id :string]
-             [:payee :string]
-             [:amount money]
-             [:comment :string]]))
-
-(defn invoice-xform [event]
-  (e/match {:event event}
-    {:event {:event-id ?event-id
-             :payee ?payee
-             :amount ?amount
-             :comment ?comment}}
-    {:comment ?comment
-     :journal-entries [{:event-id ?event-id
-                        :dr-cr :credit
-                        :account :invoices-payable
-                        :currency "USD"
-                        :local-amount ?amount}
-                       {:event-id ?event-id
-                        :dr-cr :debit
-                        :account :expenses
-                        :currency "USD"
-                        :local-amount ?amount}]}))
-
-(def rules {:share-issue {:name "Share Issuance"
-                          :schema share-issue-event
-                          :xform share-xform}
-            :invoice-payable {:name "Invoice payable"
-                              :schema invoice-payable-event
-                              :xform invoice-xform}})
-
-(def journal-entry
-  (m/schema [:map
-             [:entry-id :string]
-             [:event-id :string]
-             [:transaction-id :string]
-             [:knowledge-datetime :string]
-             [:event-date :string]
-             [:account :string]
-             [:dr-cr :string]
-             [:currency :string]
-             [:local-amount money]
-             [:book-amount money]]))
-
-
-(comment
-  (def dummy-si-event {:event-id "hello"
-                       :shares 123
-                       :price-per-share 12.21})
-  (share-xform dummy-si-event))
+(def selected-rule (r/atom :share-issue))
 
 (defn rule-dropdown []
   (fn []
@@ -100,7 +12,7 @@
      [:button.dropbtn "Select Rule"]
      [:div.dropdown-content
       (for [[k v] rules]
-        [:a {:on-click #(swap! app-state assoc :dropdown-selected k)}
+        [:a {:on-click #(reset! selected-rule k)}
          (:name v)])]]))
 
 (defn type-display [typ]
@@ -120,15 +32,16 @@
         :else "text"))
 
 (defn type-coerce [typ value]
-  (cond
-    (= :string typ) value
-    (= :int typ) (if (empty? value) 0 (js/parseInt value))
-    (= :double typ) (if (empty? value) 0 (js/parseFloat value))
-    (fn-spec? typ) (type-coerce (fn-spec-coerce typ) value)
+  (if (keyword? type)
+    (cond (= :string typ) value
+          (= :int typ) (if (empty? value) 0 (js/parseInt value))
+          (= :double typ) (if (empty? value) 0 (js/parseFloat value)))
+    (cond
+      (fn-spec? typ) (type-coerce (fn-spec-coerce typ) value)
 
-    :else "COERCE FAIL"))
+      :else "COERCE FAIL")))
 
-(defn jes->table [jes]
+(defn journal-entries->table [jes]
   [:table
    [:tr
     (for [header (keys (first jes))] [:th header])]
@@ -140,16 +53,16 @@
    [:h2 "Transaction"]
    [:p [:strong "Comment: "] (:comment transaction)]
    [:h3 "Journal Entries"]
-   (jes->table (:journal-entries transaction))])
+   (journal-entries->table (:journal-entries transaction))])
 
 (defn event-form []
   (let [event (r/atom {})]
     (fn []
       [:div
-       [:h2 (get-in rules [(:dropdown-selected @app-state) :name])]
+       [:h2 (get-in rules [@selected-rule :name])]
        [:div#event-form
         [:form {:on-submit #(.preventDefault %)}
-         (doall (for [[nm typ] (rest (m/form (get-in rules [(:dropdown-selected @app-state) :schema])))]
+         (doall (for [[nm typ] (rest (m/form (get-schema rules @selected-rule)))]
                   [:div
                    [:label {:for nm} (str (name nm) " (" (type-display typ) "): ")]
                    [:input {:type (type->input typ)
@@ -157,16 +70,16 @@
                             :step 0.01
                             :value (get @event nm)
                             :on-change #(swap! event assoc nm (type-coerce typ (-> % .-target .-value)))}]]))]
-        [:p (if (m/validate (get-in rules [(:dropdown-selected @app-state) :schema]) @event)
-              (transaction-display ((get-in rules [(:dropdown-selected @app-state) :xform]) @event))
+        [:p (if (m/validate (get-schema rules @selected-rule) @event)
+              (transaction-display ((get-xform rules @selected-rule) @event))
               "Event is not valid")]]])))
 
 (defn app []
   [:div
-   [:p (pr-str @app-state)]
+   [:p (pr-str @selected-rule)]
    [:h1 "Qniform Rule Tester"]
    [rule-dropdown]
-   [event-form share-issue-event]])
+   [event-form (get-schema rules @selected-rule)]])
 
 (defn mount []
   (rd/render [app]
---
+++ frontend/src/main/qniform/rules.cljs
@@ -0,0 +1,80 @@
+(ns qniform.rules
+  (:require [meander.epsilon :as e]
+            [malli.core :as m]))
+
+(defn round-2dp [num]
+  (/ (Math/round (* 100 num)) 100))
+
+(defn ccy-amnt? [num]
+  (and (number? num)
+       (= num (/ (Math/round (* 100 num)) 100))))
+
+(def money (m/schema [:fn {:display "Money amount"
+                           :coercion :double}
+                      ccy-amnt?]))
+
+(def share-issue-event
+  (m/schema [:map
+             [:event-id :string]
+             [:shares :int]
+             [:price-per-share money]
+             [:comment :string]]))
+
+(defn share-xform [event]
+  (e/match {:event event}
+    {:event {:event-id ?event-id
+             :shares ?shares
+             :price-per-share ?price-per-share
+             :comment ?comment}}
+    {:comment ?comment
+     :journal-entries [{:event-id ?event-id
+                        :dr-cr :credit
+                        :account :share-capital
+                        :currency "USD"
+                        :local-amount (round-2dp (* ?price-per-share ?shares))}
+                       {:event-id ?event-id
+                        :dr-cr :debit
+                        :account :cash
+                        :currency "USD"
+                        :local-amount (round-2dp (* ?price-per-share ?shares))}]}))
+
+(def invoice-payable-event
+  (m/schema [:map
+             [:event-id :string]
+             [:payee :string]
+             [:amount money]
+             [:comment :string]]))
+
+(defn invoice-xform [event]
+  (e/match {:event event}
+    {:event {:event-id ?event-id
+             :payee ?payee
+             :amount ?amount
+             :comment ?comment}}
+    {:comment ?comment
+     :journal-entries [{:event-id ?event-id
+                        :dr-cr :credit
+                        :account :invoices-payable
+                        :currency "USD"
+                        :local-amount ?amount}
+                       {:event-id ?event-id
+                        :dr-cr :debit
+                        :account :expenses
+                        :currency "USD"
+                        :local-amount ?amount}]}))
+
+(def rules {:share-issue {:name "Share Issuance"
+                          :schema share-issue-event
+                          :xform share-xform}
+            :invoice-payable {:name "Invoice payable"
+                              :schema invoice-payable-event
+                              :xform invoice-xform}})
+
+(comment
+  (def dummy-si-event {:event-id "hello"
+                       :shares 123
+                       :price-per-share 12.21})
+  (share-xform dummy-si-event))
+
+(defn get-schema [rules name] (get-in rules [name :schema]))
+(defn get-xform [rules name] (get-in rules [name :xform]))
