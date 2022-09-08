bfd386b1c18639f2bd43714c63f8dd7173c37a58
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-09-08 08:37:18 +0100

    event api endpoint

--- backend/dev/user.clj
+++ backend/dev/user.clj
@@ -1,6 +1,14 @@
 (ns user
   (:require [qniform.main :as app]
-            [org.httpkit.client :as http]))
+            [org.httpkit.client :as http]
+            [clojure.data.json :as json]))
+
+(def event (json/write-str
+            {:originated :system-x
+             :id "0b31b2a2-b144-4a9b-85cd-af99175e6a0f"
+             :type :share-issue
+             :shares 100
+             :price-per-share 12.23}))
 
 (comment
   (app/start)
@@ -14,3 +22,7 @@
 @(http/post "http://localhost:3000/readback"
             {:query-params {:hello "world"}
              :body "body test"})
+
+@(http/post "http://localhost:3000/api/event"
+            {:query-params {:hello "world"}
+             :body event})
--- backend/src/qniform/main.clj
+++ backend/src/qniform/main.clj
@@ -1,10 +1,14 @@
 (ns qniform.main
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [api-defaults wrap-defaults site-defaults]]
+            [ring.util.request :as rreq]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
             [qniform.main :as app]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.data.json :as json]
+            [malli.core :as m]
+            [qniform.rules :refer [rules get-schema get-xform]]))
 
 (defonce server (atom nil))
 @server
@@ -14,13 +18,30 @@
    :headers {"Content-Type" "text"}
    :body (with-out-str (pprint request))})
 
+(defn validate-event [event rules]
+  (if (m/validate (get-schema rules (:type event))
+                  event)
+    ((get-xform rules (:type event)) event)
+    (throw (ex-info "Event is invalid" {:explain (m/explain (get-schema rules (:type event))
+                                                            event)}))))
+
+(defn event-parser [json-event]
+  (-> json-event
+      (json/read-str :key-fn keyword)
+      (update :originated keyword)
+      (update :type keyword)
+      (validate-event rules)))
+
+(defn event-handler [request]
+  {:status  200
+   :headers {"Content-Type" "text"}
+   :body (pr-str (event-parser (rreq/body-string request)))})
+
 (defroutes routes
-  (GET "/" [] "<h1>Hello, World!</h1>")
+  (GET "/" [] "<h1>Qniform</h1>")
   (GET "/readback" [] readback)
   (POST "/readback" [] readback)
-  (GET "/api" [] {:status  200
-                  :headers {"Content-Type" "application/json"}
-                  :body "{\"hello\": \"World\"}"})
+  (POST "/api/event" [] event-handler)
   (route/not-found "<h1>Page not found</h1>"))
 
 (defn start []
