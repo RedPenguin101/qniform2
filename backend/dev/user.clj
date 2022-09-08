52f203f15e18e982344b7a25215134d886e035b3
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-09-08 10:38:17 +0100

    add user queries for share and invoice issues

--- backend/dev/user.clj
+++ backend/dev/user.clj
@@ -3,18 +3,10 @@
             [org.httpkit.client :as http]
             [clojure.data.json :as json]))
 
-(def event (json/write-str
-            {:originated :system-x
-             :id "0b31b2a2-b144-4a9b-85cd-af99175e6a0f"
-             :type :share-issue
-             :shares 100
-             :price-per-share 12.23}))
-
 (comment
   (app/start)
   (app/stop))
 
-@(http/get "http://localhost:3000/api")
 @(http/get "http://localhost:3000/")
 @(http/get "http://localhost:3000/readback?hello=world")
 @(http/get "http://localhost:3000/readback"
@@ -23,6 +15,25 @@
             {:query-params {:hello "world"}
              :body "body test"})
 
-@(http/post "http://localhost:3000/api/event"
-            {:query-params {:hello "world"}
-             :body event})
+(def share-event
+  {:originated :system-x
+   :id "0b31b2a2-b144-4a9b-85cd-af99175e6a0f"
+   :type :share-issue
+   :shares 100
+   :price-per-share 12.23
+   :comment "Test Comment"})
+
+(read-string (:body @(http/post "http://localhost:3000/api/event"
+                                {:query-params {:hello "world"}
+                                 :body (json/write-str share-event)})))
+
+(def invoice-event
+  {:originated :system-t
+   :id "eb6371ea-9cef-4a32-9b8d-abeb8cf87f20"
+   :type :invoice-payable
+   :amount 123.45
+   :payee "your mum"
+   :comment "Test Comment on invoice"})
+
+(read-string (:body @(http/post "http://localhost:3000/api/event"
+                                {:body (json/write-str invoice-event)})))
